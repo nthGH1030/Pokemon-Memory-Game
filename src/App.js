9280ea553fd0411e30726466b37f17e352de6cac
Kelvin Ngan <nth1030@gmail.com>
2024-05-21 22:57:18 +0800

    Add heading and state for score

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles.css';
+import {useState} from 'react'
+
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <p>Pokemon Memory Game</p>
+        <p>Get points by clicking on an image but dont click on anymore than once !</p>
+      </div>
+      <div>
+        Score: {score}
+        <br></br>
+        Best Record: {}
+      </div>
+
     </div>
   );
+}
+
+function Card() {
+  //Fetch random pokemon types from pokedex API, put them into an object
+
+  return (
+    <div>
+
+    </div>
+  )
 }
 
 export default App;
